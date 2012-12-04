645adc492406f315da69ca94ceb5832cdd2c1a54
Katherine Whitlock <toroidalcode@gmail.com>
2012-12-03 21:47:09 -0500

    removed some superfluous files

---
+++ home/.emacs.d/el-get/.status.el
@@ -0,0 +1,11 @@
+((color-theme status "installed" recipe
+	      (:name color-theme :description "An Emacs-Lisp package with more than 50 color themes for your use. For questions about color-theme" :website "http://www.nongnu.org/color-theme/" :type http-tar :options
+		     ("xzf")
+		     :url "http://download.savannah.gnu.org/releases/color-theme/color-theme-6.6.0.tar.gz" :load "color-theme.el" :features "color-theme" :post-init
+		     (progn
+		       (color-theme-initialize)
+		       (setq color-theme-is-global t))))
+ (el-get status "installed" recipe
+	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "4.stable" :pkgname "dimitri/el-get" :info "." :load "el-get.el"))
+ (paredit status "installed" recipe
+	  (:name paredit :description "Minor mode for editing parentheses" :type http :url "http://mumble.net/~campbell/emacs/paredit.el" :features "paredit")))
